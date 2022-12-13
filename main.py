b6632c85a12c7e3e00f2f696d17392f3da175ed6
naniduan <telub9@gmail.com>
2022-12-13 20:09:53 +0300

    the first working version

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
 import streamlit as st
 
-from PIL import Image
-import requests
-from backend import model
-
-def __main():
-    st.write("Choose an image file with a line of text:")
-    image_file = st.file_uploader("Choose an image")
-    st.write("To start, please specify whether the text is printed or handwritten.")
-    printed = st.button("printed")
-    handwritten = st.button("handwritten")
-
-    if printed:
-        st.write(model.get_printed_text(image_file))
-    if handwritten:
-        st.write(model.get_handwritten_text(image_file))
-
-if __name__ == '__main__':
-    __main()
+st.write("Work in progress")
