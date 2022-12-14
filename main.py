60aab41075b5ac858cef405ced78046535f21e2b
naniduan <telub9@gmail.com>
2022-12-14 04:22:28 +0300

    aaaaaaaaaaaaaaaaaaaaa

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
-<<<<<<< HEAD
-st.write("Work in progress")
-=======
 from PIL import Image
 import requests
 from backend import model
 
-st.write("Choose an image file with a line of text:")
-image_file = st.file_uploader("Choose an image")
-st.write("To start, please specify wether the text is printed or handwritten.")
-printed = st.button("printed")
-handwritten = st.button("handwritten")
+def __main():
+    st.write("Choose an image file with a line of text:")
+    image_file = st.file_uploader("Choose an image")
+    st.write("To start, please specify whether the text is printed or handwritten.")
+    printed = st.button("printed")
+    handwritten = st.button("handwritten")
 
-if printed:
-    st.write(model.get_printed_text(image_file))
-if handwritten:
-    st.write(model.get_handwritten_text(image_file))
->>>>>>> dd3134f (the first working version)
+    if printed:
+        st.write(model.get_printed_text(image_file))
+    if handwritten:
+        st.write(model.get_handwritten_text(image_file))
+
+if __name__ == '__main__':
+    __main()
